cbdc6efed97a6dd9da176554af7afdccd7402f6d
Kevin Han <kevinhan@Kevins-MacBook-Air.local>
2020-03-07 16:12:01 +0800

    feat: use zsh + spaceship as default shell
    Support macOS 10.15

--- home.nix
+++ home.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 let
+  NIX_PATH = "~/.nix-profile/etc/profile.d/nix.sh";
+  sourceFile = file: "[ -f ${file} ] && source ${file}";
   gui = if pkgs.stdenv.isDarwin then "darwin" else "gtk3";
 in
 {
@@ -13,7 +15,7 @@ in
     bat
     cmake
     curl
-    gcc
+    #gcc
     htop
     jq
     neofetch
@@ -21,6 +23,13 @@ in
     tree
     wget
   ];
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = false;
+    enableFishIntegration = false;
+  };
 
   programs.zsh = {
     enable = true;
@@ -32,7 +41,37 @@ in
       save = 50000;
     };
     shellAliases = import ./aliases.nix;
+    plugins = [
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.6.3";
+          sha256 = "1h8h2mz9wpjpymgl2p7pc146c1jgb3dggpvzwm9ln3in336wl95c";
+        };
+      }
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "be3882aeb054d01f6667facc31522e82f00b5e94";
+          sha256 = "0w8x5ilpwx90s2s2y56vbzq92ircmrf0l5x8hz4g1nx3qzawv6af";
+        };
+      }
+    ];
+    sessionVariables = rec {
+      EDITOR = "vim";
+      VISUAL = EDITOR;
+      GIT_EDITOR = EDITOR;
+    };
+    initExtra = ''
+      ${sourceFile NIX_PATH}
+      zsh
+    '';
   };
+
 
   programs.tmux = {
     enable = true;
